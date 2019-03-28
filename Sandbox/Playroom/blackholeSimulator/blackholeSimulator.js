617728385a4078de12abbb418cf828950918ddac
Griffin Moe <me@griffinmoe.com>
2019-03-28 07:49:01 -0500

    BH: Condense to single JS file
    For simplicity's sake, I am combining the Particle class definition with
    the main logic file. Technically these can be kept as separate files but
    there's potential for mistakes when multiple files rely on each other.
    This is better solved with tech like Browserify, although I'm not sure
    how nicely it plays with p5.js. I might have to explore this later.
    
    I also moved the `varyColor` function into the Particle class because
    that's the only place I'm using it. I'm assuming I might have started
    using it for something else when creating this sketch originally.

--- Sandbox/Playroom/blackholeSimulator/blackholeSimulator.js
+++ Sandbox/Playroom/blackholeSimulator/blackholeSimulator.js
@@ -25,6 +25,71 @@ let bgImage;
 let mouse;
 let shouldRepel = false;
 
+class Particle {
+  Particle() {
+    this.x = random(width);
+    this.y = random(height);
+    this.size = random(10,60);
+    this.fillColor = this.varyColor(particleColors[floor(random(particleColors.length))], 35);
+  }
+
+  Particle(x, y, size, fillColor) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.fillColor = fillColor;
+  }
+
+  display() {
+    fill(fillColor);
+    ellipse(this.x, this.y, this.size, this.size);
+  }
+
+  delete() {
+    this.size = 0;
+    this.x = -100;
+    this.y = -100;
+  }
+
+  varyColor(color, deviation) {
+    const red = floor(random(red(color) - deviation, red(color) + deviation));
+    const green = floor(random(green(color) - deviation, green(color) + deviation));
+    const blue = floor(random(blue(color) - deviation, blue(color) - deviation));
+    return color(red, green, blue);
+  }
+
+  distanceFrom(neighborParticle) {
+    return sqrt(
+      sq(neighborParticle.x - this.x, 2)
+      + sq(neighborParticle.y - this.y, 2)
+    );
+  }
+
+  attractTo(particle) {
+    const distance = this.distanceFrom(particle);
+    const directionX = (particle.x - x) / distance;
+    const directionY = (particle.y - y) / distance;
+    const gravitation = ((G * this.size * particle.size) / sq(distance, 2));
+
+    if (shouldRepel) {
+      this.x -= directionX * gravitation;
+      this.y -= directionY * gravitation;
+    } else {
+      this.x += directionX * gravitation;
+      this.y += directionY * gravitation;
+    }
+
+    // Blackhole swallow behavior
+    if (
+      particle.x - (particle.size / 3) <= this.x && this.x <= particle.x + (particle.size / 3)
+      && particle.y - (particle.size / 3) <= this.y && this.y <= particle.y + (particle.size / 3)
+    ) {
+      particle.size += this.size / 100;
+      this.delete();
+    }
+  }
+}
+
 function resetSystem(int numOfParticles) {
   for (let i = 0; i < numOfParticles; i += 1) {
     system[i] = new Particle();
@@ -60,11 +125,4 @@ function draw() {
   }
 
   mouse.display();
-}
-
-function varyColor(color, deviation) {
-  const red = floor(random(red(color) - deviation, red(color) + deviation));
-  const green = floor(random(green(color) - deviation, green(color) + deviation));
-  const blue = floor(random(blue(color) - deviation, blue(color) - deviation));
-  return color(red, green, blue);
 }
